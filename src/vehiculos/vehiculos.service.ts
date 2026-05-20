83284f4d0497ac82ac6601fc2d21cf5478ab187c
dualbert <dualber29@gmail.com>
2026-05-19 22:56:12 -0500

    aplicando estructura de respuesta para enpoint de busquedas all and unique

--- src/vehiculos/vehiculos.service.ts
+++ src/vehiculos/vehiculos.service.ts
@@ -1,70 +1,111 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateVehiculoDto } from './dto/create-vehiculo.dto';
 import { UpdateVehiculoDto } from './dto/update-vehiculo.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { NotFoundError } from 'rxjs';
+
 
 @Injectable()
 export class VehiculosService {
-  constructor(private prisma:PrismaService){}
+  constructor(private prisma: PrismaService) {}
 
-  async validarConductor(id_persona:string){
-    
-    const persona = await this.prisma.personas.findUnique({ where:{id: id_persona},
-    include:{
-      roles:{
-        include:{
-          rol:true,
-        }
-      }
-    }});
+  async validarConductor(id_persona: string) {
+    const persona = await this.prisma.personas.findUnique({
+      where: { id: id_persona },
+      include: {
+        roles: {
+          include: {
+            rol: true,
+          },
+        },
+      },
+    });
 
-    if(!persona){
+    if (!persona) {
       throw new NotFoundException('el conductor del vehiculo no existe');
     }
 
-    const persona_rol = persona.roles.find(n => n.rol.nombre == "Conductor");
+    const persona_rol = persona.roles.find((n) => n.rol.nombre == 'Conductor');
 
-    if(!persona_rol){
+    if (!persona_rol) {
       throw new BadRequestException(
-        'la persona seleccionada no tienen el rol de conductor'
-      )
+        'la persona seleccionada no tienen el rol de conductor',
+      );
     }
 
     return persona;
   }
 
-
-
   async create(createVehiculoDto: CreateVehiculoDto) {
-
-    await this.validarConductor(
-      createVehiculoDto.id_persona,
-    )
+    await this.validarConductor(createVehiculoDto.id_persona);
 
     return await this.prisma.vehiculos.create({
-      data:createVehiculoDto,
-    })
-
+      data: createVehiculoDto,
+    });
   }
 
-
   async findAll() {
-    return await this.prisma.vehiculos.findMany();
+    const vehiculos = await this.prisma.vehiculos.findMany({
+      include: {
+        persona: {
+          include: {
+            roles: {
+              include: {
+                rol: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return vehiculos.map((vehiculo, i) => ({
+      placa: vehiculo.placa,
+      marca: vehiculo.marca,
+      descripcion: vehiculo.descripcion,
+      capacidad: vehiculo.capacidad,
+      conductor: vehiculo.persona.nombres,
+      rol: vehiculo.persona.roles.map((r) => r.rol.nombre).join(),
+    }));
   }
 
   async findOne(placa: string) {
-    return await this.prisma.vehiculos.findUnique({ where:{placa}});
+    const vehiculo = await this.prisma.vehiculos.findUnique({
+      where: { placa },
+      include: {
+        persona: {
+          include: {
+            roles: {
+              include: {
+                rol: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return {
+      placa: vehiculo.placa,
+      marca: vehiculo.marca,
+      descripcion: vehiculo.descripcion,
+      capacidad: vehiculo.capacidad,
+      conductor: vehiculo.persona.nombres,
+      rol: vehiculo.persona.roles.map((r) => r.rol.nombre).join(),
+    };
   }
 
   async update(placa: string, updateVehiculoDto: UpdateVehiculoDto) {
     return await this.prisma.vehiculos.update({
-      data:updateVehiculoDto,
-      where:{placa},
+      data: updateVehiculoDto,
+      where: { placa },
     });
   }
 
   async remove(placa: string) {
-    return await this.prisma.vehiculos.delete({where:{placa}});
+    return await this.prisma.vehiculos.delete({ where: { placa } });
   }
 }
